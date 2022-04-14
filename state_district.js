aa508bc7c12d1c5a2c95a611fe9858d953035535
yugsharma1711 <imperialyug0805@gmail.com>
2022-04-14 15:24:01 +0530

    Created Select State Menu

--- state_district.js
+++ state_district.js
@@ -1,5 +1,6 @@
 state_menu = document.getElementById('state-menu')
 district_menu = document.getElementById('district-menu')
+state_container = document.getElementById('state')
 function state_api(){
     url = "https://cdn-api.co-vin.in/api/v2/admin/location/states"
     fetch(url)
@@ -10,12 +11,22 @@ function state_api(){
 function show_state(state_data){
     //format data['states'][number]['state_name]
     for(let i = 0 ; i < 37 ; i++){
-        let menu = document.createElement('option')
-        menu.setAttribute('id', 'state-name')
-        const text = document.createTextNode(state_data['states'][i]['state_name'])
-        menu.appendChild(text)
-        menu.value = state_data['states'][i]['state_id']
-        state_menu.appendChild(menu)
+        let state_name_container = document.createElement('div')
+        state_name_container.setAttribute('class', 'state-name-container')
+        let state_name = state_data['states'][i]['state_name']
+        let state_id = state_data['states'][i]['state_id']
+        let menu = document.createElement('input')
+        menu.setAttribute('id', state_id )
+        menu.setAttribute('type', 'radio')
+        menu.setAttribute('name', 'state-name')
+        menu.setAttribute('value', state_id)
+        let label = document.createElement('label')
+        label.setAttribute('for', state_id)
+        label.textContent = state_name
+        console.log(label.textContent)
+        state_name_container.appendChild(menu)
+        state_name_container.appendChild(label)
+        state_menu.appendChild(state_name_container)
     }
 }
 function showDistrict(district_data){
@@ -36,3 +47,12 @@ state_menu.addEventListener('change', function(){
     .then(district_data => showDistrict(district_data))
 })
 state_api()
+let find_state = document.getElementById('select-state')
+find_state.addEventListener('click', function(){
+    state_menu.style.display = 'flex'    
+})
+document.addEventListener('click', function(){
+    if(!state_container.contains(event.target)){
+        state_menu.style.display = 'none'
+    }
+})
